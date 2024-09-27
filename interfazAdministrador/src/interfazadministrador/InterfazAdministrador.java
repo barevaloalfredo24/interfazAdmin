20f53bb48609edc578cd5d35fe8fde28a46704fb
brayan <barevalod@miumg.edu.gt>
2024-09-27 16:22:40 -0600

    agregando interfaces

---
+++ interfazAdministrador/src/interfazadministrador/InterfazAdministrador.java
@@ -0,0 +1,25 @@
+
+package interfazadministrador;
+
+/**
+ *
+ * @author DELL
+ */
+public class InterfazAdministrador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+       
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
